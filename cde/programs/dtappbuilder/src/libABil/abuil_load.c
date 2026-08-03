Techokami/cdesktopenv

commit 1ac240562a0d8c87fa1030e73e27fb40e61ecf9d

dtappbuilder: fix incompatible pointer types in Uil() call
parse_uil() cast message_cb/status_cb to a local K&R-style typedef
"UIL_CB" (Uil_continue_type(*)()) before passing them to Uil(), which
actually expects the properly-prototyped uil_message_cb_t and
uil_status_cb_t types declared in <uil/Uil.h>. Like the DtMrm case,
this only used to be a warning; GCC 14 makes
-Wincompatible-pointer-types a hard error, so the build now fails.

Cast to the real typedefs instead, and drop the now-unused UIL_CB
typedef.

cde/programs/dtappbuilder/src/libABil/abuil_load.c
@@ -65,9 +65,6 @@ extern XtPointer _XmStringUngenerate (XmString string,
                         XmTextType output_type);
 
 
-typedef Uil_continue_type(*UIL_CB)();
-
-
 /*
  * Private function declarations
  */
@@ -1140,8 +1137,8 @@ parse_uil(
     /* Call uil compiler with appropriate paramters */
 
     compile_stat = Uil(&command_desc, &compile_desc,
-		       (UIL_CB)message_cb, (char *)&user_mess_data,
-		       (UIL_CB)status_cb, (char *)&user_stat_data);
+		       (uil_message_cb_t)message_cb, (char *)&user_mess_data,
+		       (uil_status_cb_t)status_cb, (char *)&user_stat_data);
 
     fprintf(stderr, "\nUIL Compiler Version %d, ",
 		compile_desc.compiler_version);

commit ecb264f9a15be26dca51063c94b0b98aa269ee7a

dtappbuilder: revert invalid UIL callback typedefs
Revert the regression introduced by commit 1ac240562a0d8c87fa1030e73e27fb40e61ecf9d.

That commit replaces the local UIL_CB callback casts with uil_message_cb_t and uil_status_cb_t, but OpenMotif 2.3.8 does not declare those types. As a result, abuil_load.c fails to compile because both callback type names are undeclared.

Restore the UIL_CB typedef and callback casts used by the public Uil interface.

The equivalent downstream reversal completed package builds for Ubuntu Noble, Resolute, and Stonking.

cde/programs/dtappbuilder/src/libABil/abuil_load.c
@@ -65,6 +65,9 @@ extern XtPointer _XmStringUngenerate (XmString string,
                         XmTextType output_type);
 
 
+typedef Uil_continue_type(*UIL_CB)();
+
+
 /*
  * Private function declarations
  */
@@ -1137,8 +1140,8 @@ parse_uil(
     /* Call uil compiler with appropriate paramters */
 
     compile_stat = Uil(&command_desc, &compile_desc,
-		       (uil_message_cb_t)message_cb, (char *)&user_mess_data,
-		       (uil_status_cb_t)status_cb, (char *)&user_stat_data);
+		       (UIL_CB)message_cb, (char *)&user_mess_data,
+		       (UIL_CB)status_cb, (char *)&user_stat_data);
 
     fprintf(stderr, "\nUIL Compiler Version %d, ",
 		compile_desc.compiler_version);